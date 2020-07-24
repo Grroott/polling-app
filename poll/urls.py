86a29024f05b1fe57465fdaea6cd4d05076a0d49
Gokul nath <gokulnath634@gmail.com>
2020-07-24 14:04:29 +0530

    Handling basic routes & views

--- poll/urls.py
+++ poll/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from . import views
+
+app_name = 'poll'
 
 urlpatterns = [
-    path('', index, name="home"),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
